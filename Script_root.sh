f72f55b36a92b82d5fc0fb5573e7ee241a5a65bb
Andrew <101985948+andrijbikov134@users.noreply.github.com>
2025-02-13 10:28:59 +0200

    Update Script_root.sh

--- Script_root.sh
+++ Script_root.sh
@@ -5,24 +5,27 @@ read username
 #git clone https://github.com/andrijbikov134/Rozetka_clone.git
 #cd ./Rozetka_clone
 
+echo "127.0.1.1 $(hostname)" | sudo tee -a /etc/hosts
+
 apt update && sudo apt upgrade -y
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install -y docker docker-ce docker-ce-cli containerd.io docker-compose
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 systemctl restart docker
 systemctl status docker
 
 echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
 sysctl -p
 
+chattr -i /etc/resolv.conf
 rm /etc/resolv.conf
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 chattr +i /etc/resolv.conf
 
 usermod -aG docker ${username}
-newgrp docker
+#newgrp docker
 
 apt install iptables-persistent -y
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
