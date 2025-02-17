10d68ec149b95a1826dd558a68ad1b80071d2923
Andrew <101985948+andrijbikov134@users.noreply.github.com>
2025-02-17 14:12:11 +0200

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,10 +50,10 @@ function App()
   const [currentUser, setCurrentUser] = useState(0);
 
   //const localhostFrontend = 'http://localhost:5173';
-  const localhostFrontend = 'https://192.168.0.113';
+  const localhostFrontend = 'https://34.57.208.213';
 
   //const localhost = 'http://localhost:8888';
-  const localhost = 'https://192.168.0.113/api';
+  const localhost = 'https://34.57.208.213/api';
 
   // Функція для разового створення JSON-файла
   const saveToLocalStorage = (key, array) =>
