e45ab387d5ec00f7a4f6435db1348d97ce925cac
Andrew <101985948+andrijbikov134@users.noreply.github.com>
2025-02-17 14:23:29 +0200

    Update Script_root.sh

--- Script_root.sh
+++ Script_root.sh
@@ -9,7 +9,7 @@ echo "127.0.1.1 $(hostname)" | sudo tee -a /etc/hosts
 
 apt update && sudo apt upgrade -y
 apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
@@ -24,10 +24,7 @@ rm /etc/resolv.conf
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 chattr +i /etc/resolv.conf
 
-usermod -aG docker ${username}
-#newgrp docker
-
-apt install iptables-persistent -y
+apt install -y iptables-persistent
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
@@ -40,7 +37,7 @@ systemctl enable iptables.service
 netfilter-persistent save
 systemctl status iptables.service
 
-apt install certbot python3-certbot-nginx
+apt install -y certbot python3-certbot-nginx
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout ./proxy/certs/selfsigned.key \
     -out ./proxy/certs/selfsigned.crt \
@@ -51,8 +48,13 @@ openssl req -newkey rsa:2048 -nodes -keyout ./database/certs/mysql-server.key \
     -subj "/CN=localhost"
 cp ./database/certs/mysql-server.crt ./database/certs/mysql-ca.pem
 
-apt install apache2-utils
+apt install -y apache2-utils
 htpasswd -c ./proxy/.htpasswd admin
 cat ./proxy/.htpasswd
 
 chown -R ${username}:${username} ../Rozetka_clone
+
+echo "END"
+
+usermod -aG docker ${username}
+newgrp docker
