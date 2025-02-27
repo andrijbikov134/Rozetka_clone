31970eb8fd4d1186c270ca42f885d14127b67441
Andrew <101985948+andrijbikov134@users.noreply.github.com>
2025-02-28 00:44:12 +0200

    Add

--- Script_root.sh
+++ Script_root.sh
@@ -37,12 +37,18 @@ systemctl enable iptables.service
 netfilter-persistent save
 systemctl status iptables.service
 
+
 apt install -y certbot python3-certbot-nginx
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout ./proxy/certs/selfsigned.key \
-    -out ./proxy/certs/selfsigned.crt \
-    -subj "/CN=localhost"
-openssl dhparam -out ./proxy/certs/dhparam.pem 2048
+
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#    -keyout ./proxy/certs/selfsigned.key \
+#    -out ./proxy/certs/selfsigned.crt \
+#    -subj "/CN=localhost"
+#openssl dhparam -out ./proxy/certs/dhparam.pem 2048
+
+certbot --nginx -d nfv.pp.ua -d www.nfv.pp.ua --config-dir ./proxy/certs/ --work-dir ./proxy/certs-work --logs-dir ./proxy/certs-logs --non-interactive --agree-tos --email andrijbikov134@gmail.com 
+
+
 openssl req -newkey rsa:2048 -nodes -keyout ./database/certs/mysql-server.key \
     -x509 -days 365 -out ./database/certs/mysql-server.crt \
     -subj "/CN=localhost"
